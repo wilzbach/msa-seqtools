b0242442f063987ee5819681affb8e879762105a
Sebastian Wilzbach <sebi@wilzbach.me>
2014-12-28 05:22:35 +0100

    recognize uniprot ids

--- test/test_seqtools.js
+++ test/test_seqtools.js
@@ -16,23 +16,41 @@ var equal = assert.deepEqual;
 // requires your main app (specified in index.js)
 var st = require('../');
 
-describe('biojs-utils-seqtools module', function(){
-  describe('#getMeta()', function(){
-    it('should split correctly', function(){
-      equal(st.getMeta("sp|abc|def"),["def", {sp: "abc"}]);
+describe('biojs-utils-seqtools module', function() {
+  describe('#getMeta()', function() {
+    it('should split correctly', function() {
+      equal(st.getMeta("sp|abc|def"), ["def", {
+        sp: "abc"
+      }]);
+    });
+    it('should should recognize uniprot', function() {
+      equal(st.getMeta("sp|abc|def a long description OS=organism GN=genename"), ["a long description", {
+        sp: "abc"
+      }, {
+        os: "organism",
+        gn: "genename",
+        en: "def"
+      }]);
     });
   });
-  describe('#contains()', function(){
-    it('should find text', function(){
+  describe('#buildLinks()', function() {
+    it('should show correct links', function() {
+      equal(st.buildLinks(st.getMeta("sp|abc|def")[1]), {
+        "Uniprot": "http://www.uniprot.org/abc"
+      });
+    });
+  });
+  describe('#contains()', function() {
+    it('should find text', function() {
       assert.ok(st.contains("abc", "a"));
     });
-    it('should not find non-existing text', function(){
+    it('should not find non-existing text', function() {
       assert.notOk(st.contains("abc", "e"));
     });
   });
-  describe('#splitNChars()', function(){
-    it('should split correctly', function(){
-      equal(st.splitNChars("abc", 2),["ab", "c"]);
+  describe('#splitNChars()', function() {
+    it('should split correctly', function() {
+      equal(st.splitNChars("abc", 2), ["ab", "c"]);
     });
   });
 });
